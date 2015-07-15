e00489066f735337f8fc945ccf823a773ddf2155
Bryan Horstmann-Allen <bryan@helium.com>
2015-07-15 14:17:58 +0000

    Move image whitelist info to a chef data bag

--- lib/Tokamak/Command/images.pm
+++ lib/Tokamak/Command/images.pm
@@ -15,21 +15,6 @@ Tokamak::Command::images - list available container images
 
 =cut
 
-
-# XXX
-# This might make more sense to store in a Chef databag, so we don't have to
-# update tokamak everywhere everytime we update an image.
-my @image_whitelist = (
-  "b67492c2-055c-11e5-85d8-8b039ac981ec", # base-64-lts    14.4.2
-  "39f29a9e-cd82-11e4-bd38-0fed3261fa5f", # elasticsearch  14.4.0
-  "5683089c-d18d-11e4-b067-9f59180479b9", # mongodb        14.4.0
-  "e312a72c-0a18-11e5-9a87-9ba4a03d4234", # postgresql     15.1.1
-  "8777db28-d302-11e4-8cf2-8793bd757e0f", # freebsd10      20150325
-  "4cbd2426-dee6-11e4-8ae3-b38f8b943cbe", # vespene        1.0.3
-  "82d952c4-1b7b-11e5-a299-bb55cb08eab1", # debian7-lx     20150625
-  "d8d81aee-20cf-11e5-8503-2bc101a1d577", # debian7        20150702
-);
-
 sub opt_spec {
   [ "all|a", "show unfiltered list of container images" ]
 }
@@ -38,7 +23,17 @@ sub validate_args {
   my ($self, $opt, $args) = @_;
 }
 
+sub image_whitelist {
+  my $image_data = qx/ knife data bag show tokamak images -Fj 2> \/dev\/null /;
+  my $json = JSON->new->utf8->pretty->allow_nonref;
+  my $images_json = $json->decode( $image_data );
+  
+  return @{$images_json->{list}};
+}
+
 sub sdc_images {
+  my @image_whitelist = @_;
+
   my $image_list = qx/ sdc-listimages /;
   my $json = JSON->new->utf8->pretty->allow_nonref;
   my $sdc_images = $json->decode( $image_list );
@@ -87,7 +82,9 @@ sub sdc_images {
 sub execute {
   my ($self, $opt, $args) = @_;
 
-  my %images = sdc_images();
+  my @image_whitelist = image_whitelist();
+
+  my %images = sdc_images(@image_whitelist);
 
   my $tb   = Text::Table->new( "UUID", "TYPE", "OS", "ROLE", "NAME", "VERSION" );
 
