10bbfb7905bbecd439af0d42a95dc76515fd1d7a
Bryan Horstmann-Allen <bryan@helium.com>
2015-08-26 22:57:58 +0000

    Don't emit acct info

--- lib/Tokamak.pm
+++ lib/Tokamak.pm
@@ -24,8 +24,8 @@ $ENV{SDC_ACCOUNT} = $config_hash->{ $default_env }->{ SDC_ACCOUNT };
 $ENV{SDC_URL}     = $config_hash->{ $default_env }->{ SDC_URL };
 $ENV{SDC_KEY_ID}  = $config_hash->{ $default_env }->{ SDC_KEY_ID };
 
-print "% " . $ENV{SDC_ACCOUNT} . " @ " . $ENV{SDC_URL} . "\n";
-print "\n";
+#print STDERR "% " . $ENV{SDC_ACCOUNT} . " @ " . $ENV{SDC_URL} . "\n";
+#print STDERR "\n";
 
 sub plugin_search_path {
   my ($self) = @_;
